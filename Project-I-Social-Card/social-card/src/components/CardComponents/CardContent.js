8e91e52b0cc7f3bcc4a81f5e3337b1ebda071b67
rajaii_a <rajaii_a@yahoo.com>
2019-05-08 15:14:42 -0400

    MVP on social card complete

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = (props) => {
+    return (
+    <div class='cardContent'>
+        <h3 class='cardh3'>Get started with React</h3>
+        <p>React makes it painless to create interactive UI's. Design simple views for each state in your application.</p>
+        <p class='p2'>reactjs.org</p>
+    </div>
+    )
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = (props) => {
+    return (
+    <div class='hTitle'>
+        <h3>Lambda School</h3>
+        <p>  @LambdaSchool   {Date.now()}</p>
+    </div>
+    )
+}
+
+export default HeaderTitle;
