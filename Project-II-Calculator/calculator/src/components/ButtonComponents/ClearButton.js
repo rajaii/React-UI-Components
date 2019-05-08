c4c45dbbf8308a424f7b4d6a590f5b0a905a2bac
rajaii_a <rajaii_a@yahoo.com>
2019-05-08 19:53:41 -0400

    added calculator, but created new file in button for display component, same file in display was giving me error

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button.css'
+
+const ClearButton = (props) => {
+    return (
+        <div class='clearFlex'>
+            <div class='clearButton'>clear</div>
+            <div class='divide'>÷</div>
+        </div>
+    )
+}
+
+export default ClearButton;
---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Button.css';
+
+const CalculatorDisplay = (props) => {
+    return <div class='displayScr'>0</div>
+}
+
+
+export default CalculatorDisplay;
