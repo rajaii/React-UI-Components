35197dfb158c9edaf33be182a3523e5fe6332579
rajaii_a <rajaii_a@yahoo.com>
2019-05-11 14:31:20 -0400

    fixed class to className and fixed entire calc directory with git bug working again

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,30 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = (props) => {
+    return (
+    <div className='buttonContainer'>
+        <div className='numberButtonFlex1'>
+            <div className="number">7</div>
+            <div className="number">8</div>
+            <div className="number">9</div>
+        </div>
+        <div className='numberButtonFlex2'>
+            <div className="number">4</div>
+            <div className="number">5</div>
+            <div className="number">6</div>
+        </div>
+        <div className='numberButtonFlex3'>
+            <div className="number">1</div>
+            <div className="number">2</div>
+            <div className="number">3</div>
+        </div>
+        <div>
+            <div className="numberZero">0</div>
+        </div>
+    </div>
+    )
+}
+
+
+export default NumberButton; 
